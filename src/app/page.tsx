e78c59d4b038704835d20a76a945d45a38e8f150
Hraby <hrabyyt@gmail.com>
2024-02-15 14:10:23 +0100

    db: sqlite and prisma orm integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import {ProgressCircle} from "@/components/progress-circle"
+import {Button} from "@nextui-org/react";
+import {Input} from "@nextui-org/react";
 
 export default function Home() {
   return (
@@ -10,10 +12,16 @@ export default function Home() {
           <a href="https://github.com/Hraby/nextjs-todo" target="_blank" className="font-light italic underline hover:font-normal hover:cursor-pointer">hraby</a>
         </div>
       </div>
-      <div className="max-w-5xl w-full flex items-center">
+      <div className="max-w-5xl w-full flex flex-col items-center">
         <div className="flex mx-auto items-center gap-x-8">
           <h1 className="text-xl mx-auto">Dnešní Todo</h1>
           <ProgressCircle totalTasks={4} completedTasks={1}/>
+        </div>
+        <div className="max-w-5xl mt-4 flex mx-auto">
+          <form action="#" method="POST" className="flex items-center gap-x-4">
+            <Input variant="bordered" type="text" label="Todo"/>
+            <Button color="secondary">Add Todo</Button>
+          </form>
         </div>
       </div>
     </main>

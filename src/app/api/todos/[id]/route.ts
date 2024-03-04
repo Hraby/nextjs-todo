f846100f8186de6d79c0dc8bbeb4d78a7f9db0c7
hraby <75084936+Hraby@users.noreply.github.com>
2024-03-04 22:00:42 +0100

    hotfix: api typesafe

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -3,13 +3,13 @@ import { prisma } from "@/db"
 import { delTodo, getTodo } from "@/lib/db";
 
 export async function DELETE(request: Request, { params }: { params: { id: string } }){
-    const todo = await delTodo(params.id)
+    const todo = await delTodo(parseInt(params.id))
     if(!todo) return NextResponse.json({"message": "Failed"}, {status: 404});
     return NextResponse.json({status: 204})
 }
 
 export async function GET(request: Request, { params }: { params: { id: string } }){
-    const todo = await getTodo(params.id)
+    const todo = await getTodo(parseInt(params.id))
     if(!todo) return NextResponse.json({"message": "Failed"}, {status: 404});
     return NextResponse.json(todo, {status: 200})
 }
